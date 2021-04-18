ca9cbb6c1b3f84b371e68ca85ac0e7cd171fd3be
twist-tracer <twist.tracer@gmail.com>
2021-04-18 16:35:07 +0300

    <lesson11> optimized task1.1

--- lesson11/task1.1.sql
+++ lesson11/task1.1.sql
@@ -8,25 +8,21 @@ CREATE TABLE `logs` (
 ENGINE = ARCHIVE;
 
 DELIMITER $$
+
 USE `shop`$$
 CREATE TRIGGER `shop`.`catalogs_AFTER_INSERT` AFTER INSERT ON `catalogs` FOR EACH ROW
 BEGIN
     INSERT INTO `shop`.`logs` (`table_name`, `ext_id`, `ext_name`) VALUES ('catalogs', NEW.id, NEW.`name`);
 END$$
-DELIMITER ;
 
-DELIMITER $$
-USE `shop`$$
 CREATE TRIGGER `shop`.`users_AFTER_INSERT` AFTER INSERT ON `users` FOR EACH ROW
 BEGIN
     INSERT INTO `shop`.`logs` (`table_name`, `ext_id`, `ext_name`) VALUES ('users', NEW.id, NEW.`name`);
 END$$
-DELIMITER ;
 
-DELIMITER $$
-USE `shop`$$
 CREATE TRIGGER `shop`.`products_AFTER_INSERT` AFTER INSERT ON `products` FOR EACH ROW
 BEGIN
     INSERT INTO `shop`.`logs` (`table_name`, `ext_id`, `ext_name`) VALUES ('products', NEW.id, NEW.`name`);
 END$$
+
 DELIMITER ;
